c04278d6bf3e57ccce2eb2302681258fb270b4de
Killian David <killian.david@etudiant.univ-reims.fr>
2023-03-23 09:06:09 +0100

    fin

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([49.26526, 4.02853], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([49.26526, 4.02853]).addTo(map)
